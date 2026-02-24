f351057dba3151fdc59134cff82384fd1324079b
Thanpisit Banyam <porsohani@gmail.com>
2026-02-24 10:59:35 +0000

    Model trained and write code to read video

--- src/test.py
+++ src/test.py
@@ -1,15 +1,43 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import cv2
+from ultralytics import YOLO
 
-# สร้างข้อมูลสุ่มด้วย NumPy
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+# 1. โหลดโมเดลตัวที่เทรนมา 100 loops (best.pt)
+ # แก้บรรทัดที่ 5 เป็นแบบนี้ (เปลี่ยน path ให้ตรงกับที่อยู่จริงของคุณ)
+model = YOLO("/app/runs/detect/1000images/train3/weights/best.pt")
+# model = YOLO("/app/runs/detect/555images/train2/weights/best.pt")
+# model = YOLO("/app/runs/detect/555images/train/weights/best.pt")
+# 2. เปิดกล้อง Webcam
+# เลข 0 คือกล้องหลักของโน้ตบุ๊ก, ถ้าต่อ Webcam นอกอาจจะเป็น 1 หรือ 2
+cap = cv2.VideoCapture(0)
 
-print(f"NumPy version: {np.__version__}")
+# ตั้งค่าความละเอียดของกล้อง (ปรับตามสเปก Webcam ของคุณ)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-# พล็อตกราฟด้วย Matplotlib
-plt.plot(x, y)
-plt.title("Singha Detection - Signal Test")
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
-plt.show() # หน้าต่างนี้จะเด้งไปโชว์ที่เครื่อง Ubuntu ของคุณ
+
+print("กด 'q' เพื่อออกจากโปรแกรม")
+
+while cap.isOpened():
+    success, frame = cap.read()
+
+    if success:
+        # 3. รันการตรวจจับ (Inference)
+        # conf=0.5: มั่นใจเกิน 50% ถึงจะแสดงผล (ปรับลดได้ถ้าถ่ายไกล 2.5 เมตรแล้วไม่ติด)
+        results = model.predict(frame, conf=0.7, stream=True)
+
+        # 4. วาดผลลัพธ์ลงบนภาพเฟรมปัจจุบัน
+        for r in results:
+            annotated_frame = r.plot()
+
+        # 5. แสดงหน้าต่าง Real-time
+        cv2.imshow("Singha Cans Detection - Real-time", annotated_frame)
+
+        # กด 'q' เพื่อปิดหน้าต่าง
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+# คืนค่าทรัพยากร
+cap.release()
+cv2.destroyAllWindows()
